29ef448be8dcc9f2d50401fe48c667284736e734
nathan-saygers <nathan.saygers@gmail.com>
2020-06-12 15:56:17 -0700

    adds favorited_problems seeds

--- database/seeds/favorited_problems.js
+++ database/seeds/favorited_problems.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('favorited_problems').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('favorited_problems').insert([
+        {id: 1, user: 1, problem_id: 1},
+        {id: 2, user: 1, problem_id: 2},
+        {id: 3, user: 2, problem_id: 3},
+        {id: 4, user: 3, problem_id: 4},
       ]);
     });
 };
