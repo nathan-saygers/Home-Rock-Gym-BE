57d3bad0e228209a2dd5c0071ea2c65bed9fa14d
nathan-saygers <nathan.saygers@gmail.com>
2020-06-12 14:18:51 -0700

    replace boilerplate with table name

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('users').insert([
         {id: 1, username: 'crush_master_08'},
         {id: 2, username: 'crimp_lord'},
         {id: 3, username: 'swing_holder'}
