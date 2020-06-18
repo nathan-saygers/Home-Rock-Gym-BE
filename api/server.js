172e6704dc170977a5b69059389942cab348769d
nathan-saygers <nathan.saygers@gmail.com>
2020-06-18 13:45:09 -0700

    Imports and calls routes

--- api/server.js
+++ api/server.js
@@ -6,6 +6,13 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 
+// Router Imports
+const appointmentsRouter = require("./endpoints/appointments/appointments-router.js");
+const holdsRouter = require("./endpoints/holds/holds-router");
+const problemsRouter = require("./endpoints/problems/problems-router");
+const sendsRouter = require("./endpoints/sends/sends-router");
+const usersRouter = require("./endpoints/users/users-router");
+
 // Server
 const server = express();
 
@@ -13,6 +20,18 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(morgan());
+
+// Router calls
+server.use("/api/appointments", appointmentsRouter);
+server.use("/api/holds", holdsRouter);
+server.use("/api/problems", problemsRouter)
+server.use("/api/sends", sendsRouter)
+server.use("/api/users", usersRouter)
+
+// API home page tester
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Let's send!" })
+})
 
 // Routes
 // ex. server.use("/api/monthly_costs", monthlyRouter);
