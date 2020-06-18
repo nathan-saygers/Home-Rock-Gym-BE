61ed97b4e0a644e909e344c19978f543a9b06077
nathan-saygers <nathan.saygers@gmail.com>
2020-06-18 13:05:11 -0700

    removes favorited_problem files and adds related models to problems model

--- api/endpoints/problems/problems-models.js
+++ api/endpoints/problems/problems-models.js
@@ -4,7 +4,10 @@ module.exports = {
   findAll,
   addProblem,
   removeProblem,
-  editProblem
+  editProblem,
+  findFavProblems,
+  addFavProblem,
+  findFavProblemById
 }
 
 function findAll() {
@@ -36,4 +39,33 @@ function editProblem(id, newProblemInfo) {
   return db("problems")
     .where({ id })
     .update(newProblemInfo)
+}
+
+// Find fav_problem by primary key
+
+function findFavProblemById(id) {
+  return db
+    .select("*")
+    .from("favorited_problems")
+    .where({ id })
+}
+
+// Query utilizing favorite_problems bridge table to show 
+// user's fav problems
+
+function findFavProblems(user_id) {
+  return db.select("p.id, fp.user")
+    .from("problems as p")
+    .join("favorite_problems as fp, fp.problem_id, p.id")
+    .where("fp.user", user_id)
+}
+
+// Function for adding entries to the favorite_problems table
+
+async function addFavProblem(user_id, problem_id) {
+  const newEntry = await db("favorited_problems")
+    .insert({user_id, problem_id})
+    .returning("id")
+
+  return findFavProblemById(newEntry[0])
 }
